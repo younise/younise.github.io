107b5c8ad8f706d26f9750f521f909034ba6a22b
Emad Younis <eyounis@gmail.com>
2026-03-16 00:22:59 -0700

    Audit cleanup: remove dead layout SCSS, fix footer, remove duplicate CSS rules, move inline style to SCSS

--- _site_dev/sw.js
+++ _site_dev/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20260315.235443';
+const cacheName = 'chirpy-20260316.002240';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
