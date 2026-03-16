57e45a258201b7384b85db7db604818659f89c3b
Emad Younis <eyounis@gmail.com>
2026-03-15 23:54:52 -0700

    Fix: remove CNAME from _site_dev, exclude CNAME from dev config

--- _site_dev/sw.js
+++ _site_dev/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20260315.231923';
+const cacheName = 'chirpy-20260315.235443';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
