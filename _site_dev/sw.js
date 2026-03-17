f7684fda68298d7fbef9a221fa33a10036205f48
Emad Younis <eyounis@gmail.com>
2026-03-16 21:06:52 -0700

    Fix: pin Stay Connected to sidebar bottom with position:sticky in all environments

--- _site_dev/sw.js
+++ _site_dev/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20260316.002240';
+const cacheName = 'chirpy-20260316.210632';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
